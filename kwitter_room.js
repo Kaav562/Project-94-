30c600de7f39a41919a6785038a991fce0d9c9d7
Kaav562 <84834886+Kaav562@users.noreply.github.com>
2021-06-24 22:35:42 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBk0GS7rvNQOSaDQXZGmOsNAXfIdNUX1zQ",
+    authDomain: "project-94-16f0c.firebaseapp.com",
+    projectId: "project-94-16f0c",
+    storageBucket: "project-94-16f0c.appspot.com",
+    messagingSenderId: "98863472089",
+    appId: "1:98863472089:web:4e163b05a9e99c8f574dc8",
+    measurementId: "G-L66PRLRCKK"
+  };
+  
